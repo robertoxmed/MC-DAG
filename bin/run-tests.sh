eb4f6b86c4a579b18f358b0affa0e3206d46b2ec
Roberto Medina <robertoxmed@gmail.com>
2016-11-17 17:47:12 +0100

    Binaries

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
 echo "========================================================================="
-echo "=             MXC Allocator test script                                 ="
+echo "=                   MXC Allocator test script                           ="
 echo "========================================================================="
 echo ""
 
-echo "Enter the number of DAGs to generate: "
+echo "Enter the number of DAGs to generate: [ENTER] "
 
 read nbdags
 
-while [[ $nbdags -lt 0 ]]; do
+while [[ $nbdags -le 0 ]]; do
   echo "Number of DAGs needs to be postive. Please insert a valid number."
   read nbdags
 done
 
-java -jar ls_alloc.jar -i ../tests/ex1.test
+echo ""
+echo "Will generate $nbdags DAGs."
+
+for i in `seq 1 $nbdags`
+do
+  java -jar generator.jar  -w 32 -e 30 -h 10 -hp 30 -o /home/roberto/workspace/LS_mxc/tests/ex$i.test -d /home/roberto/workspace/LS_mxc/tests/ex$i.dzn
+done
+
+echo ""
+echo "Running the allocation and writting to output files"
+echo ""
+
+for i in `seq 1 $nbdags`
+do
+  echo "================== Testing for file $i ========================="
+  java -jar ls_alloc.jar -i ../tests/ex$i.test -o ../tests/ex$i.out
+  echo ""
+done
