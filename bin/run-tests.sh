99cc62cacae2bd308812640ceaae54eeca9b637c
Roberto Medina <robertoxmed@gmail.com>
2016-11-25 16:43:22 +0100

    new binaries and script

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -19,7 +19,7 @@ echo "Will generate $nbdags DAGs."
 
 for i in `seq 1 $nbdags`
 do
-  java -jar generator.jar  -w 32 -e 30 -h 10 -hp 30 -o /home/roberto/workspace/LS_mxc/tests/ex$i.test -d /home/roberto/workspace/LS_mxc/tests/ex$i.dzn
+  java -jar generator.jar  -h 4 -l 4 -hl 2 -e 30 -cp 20  -o /home/roberto/workspace/LS_mxc/tests/ex$i.test
 done
 
 echo ""
