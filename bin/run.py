d435b7380ba40faaf8bf3fcc82a7c602bc1059d5
Roberto Medina <robertoxmed@gmail.com>
2017-01-10 00:04:05 +0100

    Python script

--- bin/run.py
+++ bin/run.py
@@ -2,31 +2,31 @@
 import os
 import numpy as np
 
-nbdags = 10
+nbdags = 100
 max_nb_cores = 4
-step_utilization = 0.4
-min_u_lo = 2
-min_u_hi = 2
+step_utilization = 0.5
+min_u_lo = 4
+min_u_hi = 4
 edge_proba = 20
 c_path = 30
 success = 0
 
-test_cores = [4]
+test_cores = [8]
 
 ex_root="../tests/ex"
-ouput_path = "../tests/results5.csv"
+ouput_path = "../tests/results20-1.csv"
 
-f = open("results5.csv", 'w')
+f = open("results20-1.csv", 'w')
 f.write("Cores, U LO, U HI, Success rate\n")
 
 for cores in test_cores:
-    for u_lo in np.arange(min_u_lo + 0.2, cores + 0.1 , step_utilization):
+    for u_lo in np.arange(min_u_lo, cores + 0.1 , step_utilization):
         for u_hi in np.arange(min_u_lo, cores + 0.1, step_utilization):
             dags = 0
             while (dags < nbdags):
                 u_hi_in_lo = min(u_hi,u_lo)/2
                 para = cores * 2
-                gen_command = "java -jar generator.jar -h "+str(u_hi)+" -l "+str(u_lo)+" -hl "+str(u_hi_in_lo)+" -e "+str(edge_proba)+" -cp "+str(c_path)+" -c "+str(cores)+" -p "+str(para)+" -o ../tests/ex"+str(cores)+"-"+str(dags)+"-Ulo-"+str(u_lo)+".test"
+                gen_command = "java -jar generator.jar -h "+str(u_hi)+" -l "+str(u_lo)+" -hl "+str(u_hi_in_lo)+" -e "+str(edge_proba)+" -cp "+str(c_path)+" -c "+str(cores)+" -p "+str(para)+" -o ../tests/ex"+str(cores)+"-lo-"+str(u_lo)+"-hi-"+str(u_hi)+"-e-"+str(edge_proba)+"-"+str(dags)+".test"
                 ret = os.system(gen_command)
                 if (ret == 0):
                     dags += 1
@@ -34,7 +34,7 @@ for cores in test_cores:
             print("Testing cores ", cores, " U_LO ", u_lo, " U_HI ", u_hi, " U HI in LO ", u_hi_in_lo )
 
             for dags in range (0, nbdags):
-                exe_command = "java -jar ls-alloc.jar -i "+ex_root+str(cores)+"-"+str(dags)+"-Ulo-"+str(u_lo)+".test"
+                exe_command = "java -jar ls-alloc.jar -i "+ex_root+str(cores)+"-lo-"+str(u_lo)+"-hi-"+str(u_hi)+"-e-"+str(edge_proba)+"-"+str(dags)+".test"
                 ret = os.system(exe_command)
                 if ret == 0:
                     success += 1
