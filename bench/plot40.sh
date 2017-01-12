2d55466adaeec1028d1560cec25b228e2446b320
Roberto Medina <robertoxmed@gmail.com>
2017-01-12 16:49:07 +0100

    Bench plot + results

---
+++ bench/plot40.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/gnuplot
+set term pngcairo dashed
+set output "results40.png"
+set style fill solid 1.00 border 0
+set xlabel "U HI" font "bold"
+set ylabel "Acceptance rate (%)" font "bold"
+set datafile separator ","
+
+set style line 1 lc rgb "#FF0000" lt 1 dashtype 2
+set style line 2 lc rgb "#0B610B" lt 2 dashtype 2
+set style line 3 lc rgb "#610B0B" lt 3 dashtype 2
+set style line 4 lc rgb "#29088A" lt 4 dashtype 2
+
+plot "results40-2.csv" using ($1==4 ? $2:1/0):3 lw 2 title "4" with lines,\
+"results40-2.csv" using ($1==7 ? $2:1/0):3 lw 2 title "7" with lines,\
+"results40-2.csv" using ($1==7.5 ? $2:1/0):3 lw 2 title "7.5" with lines,\
+"results40-2.csv" using ($1==8 ? $2:1/0):3 lw 2 title "8" with lines,\
+"results40-2.csv" using ($1==4 ? $2:1/0):4 ls 1 lw 2 title "4 (B)" with lines,\
+"results40-2.csv" using ($1==7 ? $2:1/0):4 ls 2 lw 2 title "7 (B)" with lines,\
+"results40-2.csv" using ($1==7.5 ? $2:1/0):4 ls 3 lw 2 title  "7.5 (B)" with lines,\
+"results40-2.csv" using ($1==8 ? $2:1/0):4 ls 4 lw 2 title "8 (B)" with lines
+pause -1
--- bench/run.py
+++ bench/run.py
@@ -2,11 +2,11 @@
 import os
 import numpy as np
 
-nbdags = 10
+nbdags = 200
 step_utilization = 0.5
 min_u_lo = 4
 min_u_hi = 4
-edge_proba = 40
+edge_proba = 60
 c_path = 30
 success = 0
 failed_baruah = 0
@@ -14,28 +14,20 @@ test_cores = [8]
 
 
 ex_root="../tests/ex"
-ouput_path = "../tests/resultsB.csv"
-f = open("resultsB.csv", 'w')
+ouput_path = "../tests/results60-2.csv"
+f = open("results60-2.csv", 'w')
 f.write("Cores, U LO, U HI, Success rate, Failed B\n")
 
 
 for cores in test_cores:
     for u_lo in np.arange(min_u_lo, cores + 0.1 , step_utilization):
         for u_hi in np.arange(min_u_lo, cores + 0.1, step_utilization):
-            dags = 0
-            while (dags < nbdags):
-                u_hi_in_lo = min(u_hi,u_lo)/2
-                para = cores * 2
-                gen_command = "java -jar generator.jar -h "+str(u_hi)+" -l "+str(u_lo)+" -hl "+str(u_hi_in_lo)+" -e "+str(edge_proba)+" -cp "+str(c_path)+" -c "+str(cores)+" -p "+str(para)+" -o ../tests/ex"+str(cores)+"-lo-"+str(u_lo)+"-hi-"+str(u_hi)+"-e-"+str(edge_proba)+"-"+str(dags)+".test"
-                ret = os.system(gen_command)
-                if (ret == 0):
-                    dags += 1
+            dags = 200
+            u_hi_in_lo = min(u_hi,u_lo)/2
             print("Testing cores ", cores, " U_LO ", u_lo, " U_HI ", u_hi, " U HI in LO ", u_hi_in_lo )
-
             for dags in range (0, nbdags):
                 exe_command = "java -jar ls-alloc.jar -i "+ex_root+str(cores)+"-lo-"+str(u_lo)+"-hi-"+str(u_hi)+"-e-"+str(edge_proba)+"-"+str(dags)+".test"
                 ret = os.system(exe_command)
-                print("Ret ", ret)
                 if (ret == 0 or ret == 5120):
                     success += 1
                 if (ret == 5120):
@@ -44,7 +36,6 @@ for cores in test_cores:
             line = "%d, %f, %f, %d, %d\n" % (cores, u_lo, u_hi, success, failed_baruah)
             f.write(line)
             success = 0
-            dags = 0
             failed_baruah = 0
 
     print(" Cores ", cores)
