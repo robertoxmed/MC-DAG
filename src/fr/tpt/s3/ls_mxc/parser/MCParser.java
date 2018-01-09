46cd974449643be7630484a60c5408659948bb76
Roberto Medina <robertoxmed@gmail.com>
2018-01-09 18:08:34 +0100

    N levels: parser for XML files

--- src/fr/tpt/s3/ls_mxc/parser/MCParser.java
+++ src/fr/tpt/s3/ls_mxc/parser/MCParser.java
@@ -68,6 +68,7 @@ public class MCParser {
 	private String[][] sHI;
 	private int hPeriod;
 	private int nbCores;
+	private int nbLevels;
 	
 	public MCParser (String iFile, String oSFile,Set<DAG> dags) {
 		setInputFile(iFile);
@@ -182,6 +183,123 @@ public class MCParser {
 	}
 	
 	/**
+	 * Reads the XML file for a N level MC System
+	 */
+	public void readXMLNlevels () {
+		try {
+			File iFile = new File(inputFile);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		
+			// 	Root element
+			Document doc = dBuilder.parse(iFile);
+			doc.getDocumentElement().normalize();
+			
+			// Extract number of cores
+			NodeList cList = doc.getElementsByTagName("cores");
+			Element c = (Element) cList.item(0);
+			setNbCores(Integer.parseInt(c.getAttribute("number")));
+			
+			// Extract number of levels
+			NodeList lList = doc.getElementsByTagName("levels");
+			Element l = (Element) lList.item(0);
+			setNbLevels(Integer.parseInt(l.getAttribute("number")));
+			
+			// Extract DAGs that constitute the system
+			NodeList eList = doc.getElementsByTagName("mcdag");
+			int count = 0;
+			
+			for (int d = 0; d < eList.getLength(); d++) {
+				Element eDag = (Element) eList.item(d);
+				DAG dag	= new DAG();
+				dag.setId(count);
+				dag.setDeadline(Integer.parseInt(eDag.getAttribute("deadline")));
+				
+				// Instantiate the DAG
+				int nb_actors = 0;
+				
+				// List of actors in the DAG
+				NodeList nList = eDag.getElementsByTagName("actor");
+				for (int i = 0; i < nList.getLength(); i++) {
+					Node n = nList.item(i);
+					if (n.getNodeType() == Node.ELEMENT_NODE) {
+						Element e = (Element) n;
+						Actor a;
+						int[] wcets = new int[getNbLevels()];
+						
+						// Initialize all the WCET of a node
+						NodeList wList = e.getElementsByTagName("wcet");
+						for (int j = 0; j < getNbLevels(); j++) {
+							Node w = wList.item(j);
+							if (w.getNodeType() == Node.ELEMENT_NODE) {
+								Element we = (Element) w;
+								
+								wcets[Integer.parseInt(we.getAttribute("number"))] = Integer.parseInt(we.getTextContent());
+							}
+						}
+						
+						a = new ActorSched(nb_actors++, e.getAttribute("name"),
+										   Integer.parseInt(e.getElementsByTagName("clo").item(0).getTextContent()),
+										   Integer.parseInt(e.getElementsByTagName("chi").item(0).getTextContent()));
+						((ActorSched) a).setGraphDead(dag.getDeadline());
+						dag.getNodes().add(a);
+					}
+				}
+				
+				// List of fault tolerance mechanisms
+				NodeList ftList = eDag.getElementsByTagName("ftm");
+				for (int i = 0; i < ftList.getLength(); i++) {
+					Node n = ftList.item(i);
+					if (n.getNodeType() == Node.ELEMENT_NODE) {
+						Element e = (Element) n;
+						if (e.getAttribute("type").contains("voter")) {
+							ActorAvail a = new ActorAvail(nb_actors++, e.getAttribute("name"),
+												Integer.parseInt(e.getElementsByTagName("clo").item(0).getTextContent()),
+												Integer.parseInt(e.getElementsByTagName("chi").item(0).getTextContent()));
+							a.setfMechanism(true);
+							a.setfMechType(ActorAvail.VOTER);
+							a.setVotTask(e.getElementsByTagName("vtask").item(0).getTextContent());
+							((ActorAvail) dag.getNodebyName(e.getElementsByTagName("vtask").item(0).getTextContent())).setVoted(true);
+							a.setNbReplicas(Integer.parseInt(e.getElementsByTagName("replicas").item(0).getTextContent()));
+							dag.getNodes().add(a);
+						} else if (e.getAttribute("type").contains("mkfirm")) {
+							ActorAvail a = (ActorAvail) dag.getNodebyName(e.getAttribute("name"));
+							a.setfMechanism(true);
+							a.setfMechType(ActorAvail.MKFIRM);
+							a.setM(Integer.parseInt(e.getElementsByTagName("m").item(0).getTextContent()));
+							a.setK(Integer.parseInt(e.getElementsByTagName("k").item(0).getTextContent()));
+							a.setVoted(true);
+						} else {
+							System.out.println("Uknown FTM");
+						}
+					}
+				}
+				
+				// List of connections
+				NodeList ports = eDag.getElementsByTagName("ports");
+				NodeList pList = ports.item(0).getChildNodes();
+				for (int i = 0; i < pList.getLength(); i++) {
+					Node n = pList.item(i);
+					if (n.getNodeType() == Node.ELEMENT_NODE) {
+						Element e = (Element) n;
+						// Creating the edge adds it to the corresponding nodes
+						@SuppressWarnings("unused")
+						Edge ed = new Edge(dag.getNodebyName(e.getAttribute("srcActor")),
+								dag.getNodebyName(e.getAttribute("dstActor")));
+					}
+				}
+				dag.sanityChecks();
+				dags.add(dag);
+				count++;
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
 	 * Writes the scheduling tables
 	 */
 	public void writeSched () throws IOException {
@@ -245,8 +363,6 @@ public class MCParser {
 		}
 	}
 	
-	
-	
 	/**
 	 * Writes the properties to check by PRISM
 	 * @throws IOException
@@ -282,8 +398,7 @@ public class MCParser {
 		} finally {
 			if (out != null)
 				out.close();
-		}
-				
+		}		
 	}
 	
 	/**
@@ -773,5 +888,13 @@ public class MCParser {
 
 	public void sethPeriod(int hPeriod) {
 		this.hPeriod = hPeriod;
+	}
+
+	public int getNbLevels() {
+		return nbLevels;
+	}
+
+	public void setNbLevels(int nbLevels) {
+		this.nbLevels = nbLevels;
 	}
 }
