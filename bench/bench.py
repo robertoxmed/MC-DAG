acc0bd303f1ff37e2ce38131bd8e0e53c14e3bb7
Roberto Medina <robertoxmed@gmail.com>
2018-09-11 17:33:45 +0200

    Minor: fixed bench script

--- bench/bench.py
+++ bench/bench.py
@@ -14,59 +14,59 @@ from email import encoders
 # Global setup for generation and benchmarks
 number_tasks = [10, 20, 50]
 number_dags = [2, 4]
-number_cores = [4, 8]
+number_cores = [4]
 edge_percentage = [20, 40]
 number_jobs = 8
-number_files = "10"
+number_files = "100"
 
 def create_setup():
     # Create the directory tree for generation
     if not os.path.exists("genned"):
         os.makedirs("genned")
-    
+
     for c in number_cores:
         if not os.path.exists("genned/c"+str(c)):
             os.makedirs("genned/c"+str(c))
-            
+
         for p in edge_percentage:
             if not os.path.exists("genned/c"+str(c)+"/e"+str(p)):
                 os.makedirs("genned/c"+str(c)+"/e"+str(p))
-            
+
             for d in number_dags:
                 if not os.path.exists("genned/c"+str(c)+"/e"+str(p)+"/"+str(d)):
                     os.makedirs("genned/c"+str(c)+"/e"+str(p)+"/"+str(d))
-                
+
                 for t in number_tasks:
                     if not os.path.exists("genned/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t)):
                         os.makedirs("genned/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t))
-    
+
     # Create the directory tree for benchmarking
     if not os.path.exists("results"):
         os.makedirs("results")
-    
+
     for c in number_cores:
         if not os.path.exists("results/c"+str(c)):
             os.makedirs("results/c"+str(c))
-    
+
         for p in edge_percentage:
             if not os.path.exists("results/c"+str(c)+"/e"+str(p)):
                 os.makedirs("results/c"+str(c)+"/e"+str(p))
-            
+
             for d in number_dags:
                 if not os.path.exists("results/c"+str(c)+"/e"+str(p)+"/"+str(d)):
                     os.makedirs("results/c"+str(c)+"/e"+str(p)+"/"+str(d))
-                
+
                 for t in number_tasks:
                     if not os.path.exists("results/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t)):
                         os.makedirs("results/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t))
                     if not os.path.exists("results/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t)+"/detail"):
                         os.makedirs("results/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t)+"/detail")
-                    
+
     print("MC-DAG script > Finished setup!")
-    
+
 def clean_generated():
     for c in number_cores:
-        for p in edge_percentage:            
+        for p in edge_percentage:
             for d in number_dags:
                 for t in number_tasks:
                     # Create the folder string
@@ -78,35 +78,35 @@ def clean_generated():
                                 os.unlink(file_path)
                         except Exception as e:
                             print(e)
-                        
+
     print("MC-DAG script > All generated files have been cleaned!\n")
 
 def generate():
-    
+
     for c in number_cores:
-        for p in edge_percentage:            
+        for p in edge_percentage:
             for d in number_dags:
                 for t in number_tasks:
                     # Vary utilization
                     low_bound = c /4
                     step = c * 0.025
                     upper_bound = c + step
-                    
+
                     for u in numpy.arange(low_bound, upper_bound, step):
                         cmd = "java -jar bin/generator.jar -mu "+str(round(u,2))+"\
                                -nd "+str(d)+" -l 2 -nt "+str(t)+" -nf "+number_files+" -e "+str(p)+"\
                                -o genned/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t)+"/test-"+str(round(u,2))+".xml\
                                -p 2 -j "+str(number_jobs)
-                               
+
                         ret = os.system(cmd)
                         if ret != 0:
                             print("MC-DAG script > ERROR unexpected behavior for the generation. Exiting...")
                             return -1
 
 def benchmark():
-    
+
     for c in number_cores:
-        for p in edge_percentage:            
+        for p in edge_percentage:
             for d in number_dags:
                 for t in number_tasks:
                     # Create the result file
@@ -117,7 +117,7 @@ def benchmark():
                     low_bound = c /4
                     step = c * 0.025
                     upper_bound = c + step
-                    
+
                     for u in numpy.arange(low_bound, upper_bound, step):
                         cmd = "java -jar bin/benchmark.jar  -i genned/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t)+"/test-"+str(round(u,2))+"*.xml\
                                -o results/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t)+"/detail/out-"+str(round(u,2))+".csv \
@@ -132,54 +132,54 @@ def main():
     usage_str = "%prog [options]"
     description_str = "Benchmark script"
     epilog_str = "Examples"
-    
+
     parser = optparse.OptionParser(usage = usage_str,
                                    description = description_str,
                                    epilog = epilog_str,
                                    add_help_option = False,
                                    version = "%prog version 0.1")
-    
+
     parser.add_option("-h", "--help", action = "store_true", dest = "help",
                       default = False, help = "Show this message and exit")
-    
+
     parser.add_option("-s", "--setup", action = "store_true", dest = "setup",
                       default = False, help = "Setup folder for generation/benchmarks")
-    
+
     parser.add_option("-g", "--generate", action = "store_true", dest = "generate",
                       default = False, help = "Launch generation")
-    
+
     parser.add_option('-b', "--benchmark", action = "store_true", dest = "benchmark",
                       default = False, help = "Launch benchmarking")
-    
+
     parser.add_option("-c", "--clean", action = "store_false", dest = "cleanup",
                       default = False, help = "Cleanup generated files")
-    
+
     (options, args) = parser.parse_args()
-    
+
     if options.help:
         print_help(parser)
         return 0
-    
+
     if options.setup:
         create_setup()
         return 0
-    
+
     if options.cleanup:
         clean_generated()
         return 0
-    
+
     if options.generate:
         generate()
-        
+
     if options.benchmark:
         benchmark()
         send_email()
-    
+
     return 0
 
 def print_help(parser):
     parser.print_help()
-    
+
 def send_email():
     i = 0
     with open("config.txt") as f:
@@ -198,11 +198,11 @@ def send_email():
     msg['From'] = "MC-DAG script"
     msg['To'] = TO
     msg['Subject'] = SUBJECT
-    
+
     msg.attach(MIMEText(TEXT, 'plain'))
-    
+
     for c in number_cores:
-        for p in edge_percentage:            
+        for p in edge_percentage:
             for d in number_dags:
                 for t in number_tasks:
                     attachment = open("results/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t)+"/out-c-"+str(c)+"-e"+str(p)+"-"+str(d)+"-"+str(t)+"-total.csv", "rb")
@@ -211,7 +211,7 @@ def send_email():
                     encoders.encode_base64(part)
                     part.add_header('Content-Disposition', "attachment; filename= results/c"+str(c)+"/e"+str(p)+"/"+str(d)+"/"+str(t)+"/out-c-"+str(c)+"-e"+str(p)+"-"+str(d)+"-"+str(t)+"-total.csv")
                     msg.attach(part)
-    
+
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.ehlo()
     server.starttls()
@@ -219,6 +219,6 @@ def send_email():
     text = msg.as_string()
     server.sendmail(FROM, TO, text)
     server.close()
-    
+
 if __name__ == "__main__":
     sys.exit(main())
